a74129e0a135bc3fc7cb912ad511da6c9559fc0b
SUJITH PEDAPATI <pedapatisujith@gmail.com>
2026-05-17 11:32:17 +0530

    Add files via upload

--- 800 rating/one_and_two.cpp
+++ 800 rating/one_and_two.cpp
@@ -3,39 +3,32 @@
 #include <algorithm>
 using namespace std;
 
-long long leftProduct(vector<int>&arr,int k){
-    long long product = 1;
-    for (int i = 0; i < k; i ++){
-        product = product * arr[i];
-    }
-    return product;
-}
-
-long long rightProduct(vector<int>&arr,int k){
-    long long product = 1;
-    for (int i = k; i < arr.size();i ++){
-        product = product * arr[i];
-    }
-    return product;
-}
-
 int main () {
     int t;
     cin >> t;
     while ( t--){
         int n;
         cin >> n;
-        vector<int>arr(n);
+        vector<long long >arr(n);
         for (int i = 0; i < n; i ++){
             cin >> arr[i];
         }
-        bool condition = false;
         int ans = -1;
-        for (int i = 1; i < n; i ++){
-            long long left_product = leftProduct(arr,i);
-            long long right_product = rightProduct(arr,i);
-            if (left_product == right_product){
-                ans = i;
+        int cnt = 0;
+        int cnt2 = 0;
+        vector<int>prefixTwo(n,0);
+        vector<int>suffixTwo(n,0);
+        prefixTwo[0] = (arr[0] == 2);
+        for (int i = 1 ; i < n; i ++){
+            prefixTwo[i] = prefixTwo[i-1] + ( arr[i] == 2);
+        }
+        suffixTwo[n - 1] = (arr[n - 1] == 2);
+        for (int i = n-2; i >= 0; i --){
+            suffixTwo[i] = suffixTwo[i+1] + ( arr[i] == 2);
+        }
+        for (int i = 0;  i < n-1; i ++){
+            if (prefixTwo[i] == suffixTwo[i+1]) {
+                ans = i + 1;
                 break;
             }
         }

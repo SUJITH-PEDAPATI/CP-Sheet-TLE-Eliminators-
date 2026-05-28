6d24295879b642b96f4faacec89b1951d1a7fe4e
SUJITH PEDAPATI <pedapatisujith@gmail.com>
2026-05-28 10:19:54 +0530

    Add files via upload

---
+++ 900 rating/vasilije_in_Cacak.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+int main (){
+    int t;
+    cin >> t;
+    while ( t-- ){
+        long long n,k,x;
+        cin >> n >> k >> x;
+        long long  minSum = (k*(k+1)/2);
+        long long maxSum = ((k)*(2*n - k + 1)/2);
+        if ( (x >= minSum) && ( x <= maxSum)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+
+    }
+}

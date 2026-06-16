b94175d7fe71ac73d680a5c69c81b2dc45528e33
SUJITH PEDAPATI <pedapatisujith@gmail.com>
2026-06-16 10:16:44 +0530

    Add files via upload

---
+++ 900 rating/permutation_swap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        int k = abs(a[0] - 1);
+
+        for (int i = 1; i < n; i++) {
+            k = gcd(k, abs(a[i] - (i + 1)));
+        }
+
+        cout << k << '\n';
+    }
+
+    return 0;
+}
